acebdc3be1020816f3f0fe3d051b708292bc5a69
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-25 21:33:49 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Arrays Debug
+ * Pawelski
+ * 1/25/2022
+ * This program does not work! Debug this program so that it
+ * displays all the words in the list to the console with each
+ * word on its own line.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {"apple", "orange", "grape", "watermelon",
+                                "lime", "lemon", "pear", "pineapple", "banana"};
+            for(int i = 1; i <= words.Length; i++)
+            {
+                Console.Write(words[i]);
+            }
+        }
+    }
+}
